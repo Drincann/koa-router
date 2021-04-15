8046688cd42b310b78b5d0dc28feff6c976e4a64
GaoLiHai <1019933576@qq.com>
2021-04-15 17:56:33 +0800

    chore: babel

---
+++ dist/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+class Router {
+  constructor() {
+    this.getRouteMap = new Map();
+    this.postRouteMap = new Map();
+  }
+
+  get(route, func) {
+    this.getRouteMap.set(route.trim(), func);
+  }
+
+  post(route, func) {
+    this.postRouteMap.set(route.trim(), func);
+  }
+
+  middleware() {
+    return (async (ctx, next) => {
+      if (ctx.method == 'GET') {
+        if (this.getRouteMap.has(ctx.url)) {
+          return await this.getRouteMap.get(ctx.url)(ctx, next);
+        }
+      } else if (ctx.method == 'POST') {
+        if (this.postRouteMap.has(ctx.url)) {
+          return await this.postRouteMap.get(ctx.url)(ctx, next);
+        }
+      }
+
+      await next();
+    }).bind(this);
+  }
+
+}
+
+module.exports = Router;
